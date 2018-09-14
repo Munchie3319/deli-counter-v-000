04594994fe3c2d48ba6619d233ea6079a8d6ef4f
rosa lee <rosalee0116@gmail.com>
2018-09-14 00:09:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line(katz_deli)
+  line_array = []
+  if katz_deli.length == 0
+    puts "The line is currently empty"
+  else
+    katz_deli.each.with.index(1) do |value, index|
+      line_array.push("#{index}. #{value}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
+  end
+end
